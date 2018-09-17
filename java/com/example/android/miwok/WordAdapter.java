7840e63373f0a1a7933cd49fd8cb12100530d443
Adithi Krishnan <38013049+adithikrishnan@users.noreply.github.com>
2018-09-17 15:38:33 +0530

    Update WordAdapter.java

--- java/com/example/android/miwok/WordAdapter.java
+++ java/com/example/android/miwok/WordAdapter.java
@@ -10,6 +10,24 @@ public class WordAdapter extends ArrayAdapter<Word> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+             }
+        
+        Word currentWord = getItem(position);
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+        TextView defaultTextView = (TextView) listItemView.findViewById(default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        // ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+        //iconView.setImageResource(currentAndroidFlavor.getImageResourceId());
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
     }
 }
