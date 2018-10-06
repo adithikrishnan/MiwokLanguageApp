7d8a967da14ae848fcf2b8463ab88ed443068f2e
Adithi Krishnan <38013049+adithikrishnan@users.noreply.github.com>
2018-10-06 21:08:19 +0530

    Update WordAdapter.java

--- java/com/example/android/miwok/WordAdapter.java
+++ java/com/example/android/miwok/WordAdapter.java
@@ -26,11 +26,6 @@ public class WordAdapter extends ArrayAdapter<Word> {
         TextView defaultTextView = (TextView) listItemView.findViewById(default_text_view);
         defaultTextView.setText(currentWord.getDefaultTranslation());
 
-        // ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
-        //iconView.setImageResource(currentAndroidFlavor.getImageResourceId());
-
-        // Return the whole list item layout (containing 2 TextViews and an ImageView)
-        // so that it can be shown in the ListView
         return listItemView;
     }
 }
