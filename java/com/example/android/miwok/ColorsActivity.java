fbc4d0510ee1ff94a2ff5e9499492b8f51404a51
Adithi Krishnan <38013049+adithikrishnan@users.noreply.github.com>
2018-10-06 17:16:11 +0530

    Update ColorsActivity.java

--- java/com/example/android/miwok/ColorsActivity.java
+++ java/com/example/android/miwok/ColorsActivity.java
@@ -9,5 +9,21 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+        
+         ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+        
+        WordAdapter adapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
     }
 }
