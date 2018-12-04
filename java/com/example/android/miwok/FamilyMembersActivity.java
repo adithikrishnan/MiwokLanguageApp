aae9ef0789846805b6e2f410b018eb74e5c27886
Adithi Krishnan <38013049+adithikrishnan@users.noreply.github.com>
2018-12-04 17:47:43 +0530

    Update FamilyMembersActivity.java

--- java/com/example/android/miwok/FamilyMembersActivity.java
+++ java/com/example/android/miwok/FamilyMembersActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 
 public class FamilyMembersActivity extends AppCompatActivity {
     private MediaPlayer mMediaPlayer;
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp){
+                releaseMediaPlayer();
+            }	
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,11 +40,20 @@ public class FamilyMembersActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemCLick(AdapterView<?> parent, View view, int position, long id) {
-                Word word = words.get(position)
+                Word word = words.get(position);
+                releaseMediaPlayer();
                 mMediaPlayer = MediaPlayer.create(FamilyMembersActivity.this, word.getAudioResourceID());
                 mMediaPlayer.start();
             }
         });
-
+        
+        mMediaPlayer.setOnCompletionListener(mCompletionListener);
     }
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
+        mMediaPlayer.release();
+        mMediaPlayer = null;
+    }
+}
+
 }
